b68351eacf521f4010aa5c15d0cc8c8c962c79f9
fiska0210 <asdzxc2671@gmail.com>
2018-10-10 00:34:27 +0800

    init

--- hw1/hw1-1.py
+++ hw1/hw1-1.py
@@ -5,8 +5,7 @@ def conventional_rgb2gray(bgr):
     convent = np.array([0.114,0.587,0.299]).transpose()  #cv2 read by "BGR" 
     print(convent)
     img_y = np.dot(bgr,convent)
-    print(img_y)
-
+    #print(img_y)
     cv2.imwrite('test1.png', img_y)
     cv2.imshow("Result:", cv2.imread('test1.png'))
     cv2.waitKey(0)
